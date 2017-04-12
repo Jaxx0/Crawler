eeeb07a894fce6c2556e41eac946d43f62564ffc
Jaxx0 <jackson.kitili@gmail.com>
2017-04-12 21:15:50 +0300

    BeutifulSouped it

--- scraptest.py
+++ scraptest.py
@@ -1,3 +1,5 @@
-from urllib.request import urlopen
-html = urlopen("http://www.trufece.org/")
-print(html.read())
+from urllib import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/")
+Bsobj = BeautifulSoup(html.read(), "lxml")
+print(Bsobj)
