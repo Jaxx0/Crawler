ebf5144d2cb1124374b3d493e3caa19a29a4b2e0
Jaxx0 <jackson.kitili@gmail.com>
2017-04-14 17:03:22 +0300

    Added Error handling

--- AdvancedHtmlParsing.py
+++ AdvancedHtmlParsing.py
@@ -1,7 +1,22 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("https://www.olx.co.ke/ad/samsung-galaxy-a520-gold-ID15QNYa.html")
-bsobj = BeautifulSoup(html, "lxml")
-namelist = bsobj.findAll("meta", {"name":"description"})
-for name in namelist:
-    print(name) 
+def get_namelist(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsobj = BeautifulSoup(html, "lxml")
+        namelist = bsobj.find("table", {"class":"details fixed marginbott20 margintop5 full"})
+    except AttributeError as e:
+        return None
+    return namelist
+
+namelist = get_namelist("https://www.olx.co.ke/ad/toyota-landcruiser-prado-ID15QKk3.html#clm-ke_r2-2_id1002599911")
+if namelist == None:
+    print("Info could not be found")
+else:
+    for name in namelist:
+        print(name)
+
